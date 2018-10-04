30c3ae11d236472ba569c4da631736d0aa620a26
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-04 14:27:46 +0300

    Переделал основное задание

--- Lesson1/standartTask/js/script.js
+++ Lesson1/standartTask/js/script.js
@@ -1,23 +1,24 @@
-"use strict";
+ "use strict";
 
-let budget,
-    shopName;
+let money,
+    time;
 
-budget = +prompt('Ваш бюджет на месяц?', '');
-shopName = prompt('Название вашего магазина?', '');
+money = +prompt('Ваш бюджет на месяц?', '');
+time = prompt('Введите дату в формате YYYY-MM-DD', '');
 console.log(typeof(budget));
 let mainList = {
-    budget: budget,
-    shopName: shopName,
-    shopGoods: [],
-    employers: {},
-    open: true
+    money: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
-for (var i = 0; i < 3; i++) {
-    mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать?', '');
-}
+let article = prompt('Введите обязательную статью расходов в этом месяце', '');
+let cost = prompt('Во сколько обойдется?', '');
+mainList.expenses[article] = cost;
 
-alert('Ваш бюджет на день: ' + mainList.budget/30);
+alert('Ваш бюджет на день: ' + mainList.money/30);
 console.log(mainList);
 
