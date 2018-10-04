f938ee2837035493ce4c45936bfa65953840b0ad
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-04 16:45:42 +0300

    Добавлен код в функции

--- Lesson1/extraTask/js/script.js
+++ Lesson1/extraTask/js/script.js
@@ -1,14 +1,18 @@
-"use strict";
+function main() {
+    "use strict";
 
-let num = 33721,
-    prod = 1;
+    let num = 33721,
+        prod = 1;
 
-while (num>0) {
-    prod = prod * (num % 10);
-    num = (num - num % 10) / 10;
+    while (num>0) {
+        prod = prod * (num % 10);
+        num = (num - num % 10) / 10;
+    }
+
+    prod = prod ** 3;
+
+    prod = prod + '';
+    alert(prod[0] + prod[1]);
 }
 
-prod = prod ** 3;
-
-prod = prod + ''
-alert(prod[0] + prod[1]);
+main();
--- Lesson1/standartTask/js/script.js
+++ Lesson1/standartTask/js/script.js
@@ -1,24 +1,29 @@
- "use strict";
-
-let money,
+function main() {
+    "use strict";
+    let money,
     time;
 
-money = +prompt('Ваш бюджет на месяц?', '');
-time = prompt('Введите дату в формате YYYY-MM-DD', '');
-console.log(typeof(budget));
-let mainList = {
-    money: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
-};
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    let mainList = {
+        money: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false
+    };
+    let article;
+    let cost;
+    article = prompt('Введите обязательную статью расходов в этом месяце', '');
+    cost = prompt('Во сколько обойдется?', '');
+    mainList.expenses[article] = cost;
+    article = prompt('Введите обязательную статью расходов в этом месяце', '');
+    cost = prompt('Во сколько обойдется?', '');
+    mainList.expenses[article] = cost;
 
-let article = prompt('Введите обязательную статью расходов в этом месяце', '');
-let cost = prompt('Во сколько обойдется?', '');
-mainList.expenses[article] = cost;
+    alert('Ваш бюджет на день: ' + mainList.money/30);
+    console.log(mainList); 
+}
 
-alert('Ваш бюджет на день: ' + mainList.money/30);
-console.log(mainList);
-
+main();
