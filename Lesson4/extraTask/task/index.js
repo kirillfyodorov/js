73632c494d0a5a11e19013447a0e357e640015d3
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-09 22:30:34 +0300

    Добавил доп задание

---
+++ Lesson4/extraTask/task/index.js
@@ -0,0 +1,39 @@
+function isInteger(num) {
+    return (num ^ 0) === num;
+}
+
+function getFriendlyNumber(start, end) {
+    if (typeof (start) != 'number' || typeof (end) != 'number' || end < start || end < 2 || start < 1 || !isInteger(start) || !isInteger(end)) {
+        return(false);
+    } else {
+        let arr = [];
+        let mas = [];
+        for (let i = start; i <= end; i++) {
+            mas[i] = 0;
+            for (let j = 1; j <= (i / 2 + 1); j++) {
+                if (i % j == 0) {
+                    mas[i] = mas[i] + j;
+                }
+            }
+        }
+        let i = start;
+        let j;
+        while (i < end) {
+            j = i + 1;
+            while (j <= end) {
+                if ((mas[i] == j) && (mas[j] == i)) {
+                    arr.push([i, j]);
+                }
+                j++;
+            }
+            i++;
+        }
+        return (arr);
+    }
+}
+
+module.exports = {
+    firstName: 'Kirill',
+    secondName: 'Fyodorov',
+    task: getFriendlyNumbers
+}
