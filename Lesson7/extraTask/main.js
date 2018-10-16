7322ef376540e374d0687bfcb6a69a3730a76dd0
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-16 19:21:48 +0300

    Добавил доп задание

--- Lesson7/extraTask/main.js
+++ Lesson7/extraTask/main.js
@@ -2,43 +2,39 @@ let div = document.querySelectorAll('.div'),
     btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
-    let pos = 0;
-    let w = 50;
-    let timer = setInterval(frame, 1);
-    div[0].style.width = w + 'px';
-    div[1].style.height = w + 'px';
-    div[2].style.width = w + 'px';
-    div[3].style.height = w + 'px';
-    function frame() {
-        if (pos == 350) {
-            if (w == 750) {
-                clearInterval(timer);
-            } else {
-                w++;
-                div[0].style.width = w + 'px';
-                div[1].style.height = w + 'px';
-                div[2].style.width = w + 'px';
-                div[2].style.left = 750 - w + 'px';
-                div[3].style.height = w + 'px';
-                div[3].style.top = 750 - w + 'px';
-            }
-        } else {
-            pos++;
-            div.forEach(function(elem) {
+    animate(function (timePassed) {
+        if (timePassed < 353) {
+            div.forEach(function (elem) {
                 elem.style.display = 'block';
             });
-            div[0].style.top = 375 - pos + 'px';
-            div[0].style.left = 375 - pos + 'px';
+            div[0].style.top = 375 - timePassed + 'px';
+            div[0].style.left = 375 - timePassed + 'px';
 
-            div[1].style.top = 375 - pos + 'px';
-            div[1].style.left = 350 + pos + 'px';
+            div[1].style.top = 375 - timePassed + 'px';
+            div[1].style.left = 350 + timePassed + 'px';
 
-            div[3].style.top = 350 + pos + 'px';
-            div[3].style.left = 375 - pos + 'px';
+            div[3].style.top = 350 + timePassed + 'px';
+            div[3].style.left = 375 - timePassed + 'px';
 
-            div[2].style.top = 350 + pos + 'px';
-            div[2].style.left = 350 + pos + 'px';
+            div[2].style.top = 350 + timePassed + 'px';
+            div[2].style.left = 350 + timePassed + 'px';
         }
-        
-    }
+    }, 353);
 });
+
+    function animate(draw, duration) {
+        let start = performance.now();
+
+        requestAnimationFrame(function animate(time) {
+            let timePassed = time - start;
+            if (timePassed > duration) {
+                timePassed = duration;
+            }
+            draw(timePassed);
+
+            if (timePassed < duration) {
+                requestAnimationFrame(animate);
+            }
+
+        });
+    }
