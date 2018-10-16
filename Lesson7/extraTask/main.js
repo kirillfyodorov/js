c5de15858efdcfa046fde7dd7ae1a38a6f9ad04c
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-16 21:33:43 +0300

    3 вариант доп задания

--- Lesson7/extraTask/main.js
+++ Lesson7/extraTask/main.js
@@ -1,40 +1,26 @@
 let div = document.querySelectorAll('.div'),
     btn = document.querySelector('button');
 
-btn.addEventListener('click', function() {
-    animate(function (timePassed) {
-        if (timePassed < 353) {
-            div.forEach(function (elem) {
-                elem.style.display = 'block';
-            });
-            div[0].style.top = 375 - timePassed + 'px';
-            div[0].style.left = 375 - timePassed + 'px';
+let pos = 0;
 
-            div[1].style.top = 375 - timePassed + 'px';
-            div[1].style.left = 350 + timePassed + 'px';
+function step() {
+    let request = requestAnimationFrame(step);
+    div.forEach(function (elem) {
+        elem.style.display = 'block';
+    });
+    pos += 2.5;
+    div[0].style.top = 375 - pos + 'px';
+    div[0].style.left = 375 - pos + 'px';
+    div[1].style.top = 375 - pos + 'px';
+    div[1].style.left = 350 + pos + 'px';
+    div[2].style.top = 350 + pos + 'px';
+    div[2].style.left = 350 + pos + 'px';
+    div[3].style.top = 350 + pos + 'px';
+    div[3].style.left = 375 - pos + 'px';
 
-            div[3].style.top = 350 + timePassed + 'px';
-            div[3].style.left = 375 - timePassed + 'px';
-
-            div[2].style.top = 350 + timePassed + 'px';
-            div[2].style.left = 350 + timePassed + 'px';
-        }
-    }, 353);
-});
-
-    function animate(draw, duration) {
-        let start = performance.now();
-
-        requestAnimationFrame(function animate(time) {
-            let timePassed = time - start;
-            if (timePassed > duration) {
-                timePassed = duration;
-            }
-            draw(timePassed);
-
-            if (timePassed < duration) {
-                requestAnimationFrame(animate);
-            }
-
-        });
+    if (pos == 350) {
+        cancelAnimationFrame(request);
     }
+}
+
+btn.addEventListener('click', step);
