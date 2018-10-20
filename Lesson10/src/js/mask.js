621e133ce6809624973ce2eac9ffe1c5760e0b02
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-20 19:37:39 +0300

    Правки по маске

--- Lesson10/src/js/mask.js
+++ Lesson10/src/js/mask.js
@@ -6,27 +6,31 @@ window.addEventListener('DOMContentLoaded', function () {
         input.value = '+7 (';
         input.addEventListener('keyup', (e) => {
             let s = input.value.slice(-1);
-            if (e.keyCode == 8) {
-                k--;
-                if (k < 5) {
-                    input.value = '+7 (';
-                    k = 4;
+            if (input.value.length > 18 ) {
+                input.value = input.value.slice(0, input.value.length - 1);
+            } else {
+                if (e.keyCode == 8) {
+                    k--;
+                    if (k < 5) {
+                        input.value = '+7 (';
+                        k = 4;
+                    }
                 }
-            }
-            else {
-                if (!isNaN(s) && s != ' ') {
-                input.value = input.value.slice(0, k) + s + input.value.slice(k + 1, input.value.length - 1);
-                k++;
-                if (k == 7) {
-                    input.value = input.value + ") ";
-                    k = k + 2;
-                }
-                if (k == 12 || k == 15) {
-                    input.value = input.value + " ";
+                else {
+                    if (!isNaN(s) && s != ' ') {
+                    input.value = input.value.slice(0, k) + s + input.value.slice(k + 1, input.value.length - 1);
                     k++;
-                }
-                } else {
-                    input.value = input.value.slice(0, input.value.length - 1);
+                    if (k == 7) {
+                        input.value = input.value + ") ";
+                        k = k + 2;
+                    }
+                    if (k == 12 || k == 15) {
+                        input.value = input.value + " ";
+                        k++;
+                    }
+                    } else {
+                        input.value = input.value.slice(0, input.value.length - 1);
+                    }
                 }
             }
             
