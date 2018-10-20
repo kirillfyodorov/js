76423478cfdd2aa71bd7159d3581399a74edf083
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-20 20:21:42 +0300

    Правки по маске

--- Lesson10/src/js/mask.js
+++ Lesson10/src/js/mask.js
@@ -2,11 +2,19 @@ window.addEventListener('DOMContentLoaded', function () {
     let input = document.querySelector('input');
     input.value = '+7 (ХХХ) ХХХ ХХ ХХ';
     let k = 4;
+    let range = document.createRange();
+
     input.addEventListener('focus', () => {
-        input.value = '+7 (';
-        input.addEventListener('keyup', (e) => {
+        if (input.value.length == 18) {
+            input.value = '+7 ()'
+        }
+        if (input.value.length == 5) {
+            input.setSelectionRange(4, 5);
+        }
+        input.addEventListener('keypress', (e) => {
+            
             let s = input.value.slice(-1);
-            if (input.value.length > 18 ) {
+            if (input.value.length > 17 ) {
                 input.value = input.value.slice(0, input.value.length - 1);
             } else {
                 if (e.keyCode == 8) {
