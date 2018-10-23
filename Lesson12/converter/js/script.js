07be2cacad183e1b65a3b2da6e457a69805dd043
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-23 18:32:09 +0300

    Добавил доп задание

---
+++ Lesson12/converter/js/script.js
@@ -0,0 +1,34 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+let request;
+
+inputRub.addEventListener('input', () => {
+    request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', function () {
+        function sendData() {
+            return new Promise(function (resolve, reject) {
+                if (request.readyState === 4 && request.status == 200) {
+                    resolve();
+                    console.log(request.readyState);
+                } else {
+                    reject();
+                    console.log(request.readyState);
+                }
+            });
+        }
+        sendData()
+            .then(() => {
+                let data = JSON.parse(request.response);
+                inputUsd.value = inputRub.value / data.usd;
+            })
+            .catch(() => {
+                inputUsd.value = "Что-то пошло не так!";
+            });
+    });
+        
+});
