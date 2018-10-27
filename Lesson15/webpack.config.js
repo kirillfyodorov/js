5943a15a478bd660cd5479d5afd984ed9a756b7d
Kirill <kirill-fjodorov00000@yandex.ru>
2018-10-27 13:49:54 +0300

    Удалил папку node-modules

--- Lesson15/webpack.config.js
+++ Lesson15/webpack.config.js
@@ -13,32 +13,32 @@ module.exports = {
     watch: true,
     devtool: "sourse-map",
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                include: [],
-                use: {
-                    loader: 'babel-loader?optional[]=runtime',
-                    options: {
-                        presets: [
-                            ["@babel/env", {
-                                targets: {
-                                    edge: '17',
-                                    firefox: '60',
-                                    chrome: '67',
-                                    safari: '11.1',
-                                    ie: '11'
-                                },
-                                useBuiltIns: 'usage'
-                            }]
-                        ]
-                    }
-                }
-            }
-        ]
-    }
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js$/,
+    //             exclude: /(node_modules|bower_components)/,
+    //             include: [],
+    //             use: {
+    //                 loader: 'babel-loader?optional[]=runtime',
+    //                 options: {
+    //                     presets: [
+    //                         ["@babel/env", {
+    //                             targets: {
+    //                                 edge: '17',
+    //                                 firefox: '60',
+    //                                 chrome: '67',
+    //                                 safari: '11.1',
+    //                                 ie: '11'
+    //                             },
+    //                             useBuiltIns: 'usage'
+    //                         }]
+    //                     ]
+    //                 }
+    //             }
+    //         }
+    //     ]
+    // }
     // plugins: [
     //     new UglifyJsPlugin()
     // ]
